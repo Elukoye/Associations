85673bba09b935c1a045447ed64134b53f049911
Elukoye <lukoyedith@gmail.com>
2019-12-01 19:47:51 +0000

    added index view & edit view , index and edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -15,6 +19,21 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end 
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:success]='Your Profile has been successfully Updated!'
+      redirect_to @user 
+    else
+      render 'new'
+    end
   end
   
   private
