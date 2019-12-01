1091c43f40cdcb76b26bed76b48a22109987276b
Elukoye <lukoyedith@gmail.com>
2019-12-01 18:10:49 +0000

    displayed error messages for validation,added view for show , added show action for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,34 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Your account was successfully created :)'
+      redirect_to @user
+    else
+     render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end
+  
 end
+
+
+
+
+
+
+
+
+
+
