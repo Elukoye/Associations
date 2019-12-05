8466fd720ebecbd1af201844b40e3a6f44034339
Anna Myzukina <anna.muzykina83@gmail.com>
2019-12-05 18:55:07 +0200

    Add styles and methods to create form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+      log_in @user
+      redirect_to @user
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
                             uniqueness: { case_sensitive: false }
           has_secure_password
           validates :password, presence: true, length: { minimum: 6 }
+
+          def User.digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                          BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+          end
 end
