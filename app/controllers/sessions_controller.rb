f91c8976c617e305d760f0115641e5864bc26831
Anna Myzukina <anna.muzykina83@gmail.com>
2019-12-09 17:14:06 +0200

    Add links to footer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       flash[:success]='You have successfully logged in!'
-      log_in(user)
-      redirect_to user_path(user)
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
