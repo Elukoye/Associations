0669957d05b3bb47d043744591c03bf3c0dc3724
Elukoye <lukoyedith@gmail.com>
2019-12-10 17:42:46 +0000

    modified show.html.erb in events,modified events controller and event.rb -model file

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,70 +1,59 @@
 class EventsController < ApplicationController
-  before_action :logged_in_user, only: %i[new create edit update destroy]
-  before_action :correct_user, only: %i[edit update destroy]
+    before_action :logged_in?, only: [ :new, :create, :edit, :update, :destroy]
+	before_action :find_event, only: [:show, :edit, :update,  :destroy]
 
-  def index
-    @future_events = Event.future_events
-    @past_events = Event.past_events
-  end
+	def index
+		@events = Event.all.paginate(page: params[:page])
+		#@events_upcoming = Event.upcoming.paginate(page: params[:upcoming])
+		#@events_past = Event.past.paginate(page: params[:past])
+	end
 
-  def new
-    @event = current_user.events.build 
-  end
+	def new
+		@event = current_user.events.build
+	end
 
-  def create
-    @event = current_user.events.build(event_params)
-    if @event.save
-      flash[:success] = 'Your event has been created.'
-      redirect_to @event
-    else
-      render 'new'
-    end
-  end 
-  
-  def show
-   @event = Event.find(params[:id])
-  end
+	def create
+		@event = current_user.events.build(event_params)
+		if @event.save
+			flash.now[:success] = "Your event was created successfully."
+			redirect_to event_path(@event)
+		else
+			render 'new'
+		end
+	end
 
-  def edit
-   @event = Event.find(params[:id])
-  end
+	def show
+		# Uses find_event method
+	end
 
-  def update
-    @event = Event.find(params[:id])
-    if @event.update_attributes(event_params)
-      flash[:success] = 'Your event has been updated.'
-      redirect_to @event
-    else
-      render 'edit'
-    end
-  end
+	def edit
+		# Uses find_event method
+	end
 
-  def destroy
-    @event = Event.find(params[:id])
-    @event.destroy
-    flash[:success] = 'Event has been cancelled.'
-    redirect_to root_path
-  end
+	def update
+		if @event.update(event_params)
+			flash[:success] = "Your event was updated successfully"
+			redirect_to @event
+		else
+			render 'new'
+		end
+	end
 
-  private
+	def destroy
+		@event.destroy
+		flash[:danger] = "Your event was DELETED successfully!"
+		redirect_to root_path
+	end
 
-  def event_params
-    params.require(:event).permit(:location, :description, :date)
-  end
+	private
 
-  def logged_in_user
-    unless logged_in?
-      flash[:info] = 'You must be logged in to do that.'
-      redirect_to login_path
-    end
-  end
+		def find_event
+			@event = Event.find(params[:id])
+		end
 
-  def correct_user
-    @user = Event.find(params[:id]).creator
-    unless @user == current_user
-      flash[:danger] = 'You are not authorized.'
-      redirect_to root_path
-    end
-  end
+		def event_params
+			params.require(:event).permit(:title, :description, :location, :date )
+		end
+
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, :class_name => "User"
-  has_many :attendees, :through => :event_attendees 
   has_many :event_attendees, :foreign_key => :attended_event_id
+  has_many :attendees, :through => :event_attendees 
+ 
 
   scope :upcoming, -> { where("Date >= ?", Date.today).order('Date ASC') } 
   scope :past, -> { where("Date < ?", Date.today).order('Date DESC') }
