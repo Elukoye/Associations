0dcf2204e8c9fe3f6d04f3ef3ed39714865c1cfe
Elukoye <lukoyedith@gmail.com>
2019-12-20 12:10:54 +0000

    added unit and integration tests to the site

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,39 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
   
+  test "user should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = " "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+  
+   test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end 
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,12 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def log_in user
+    session[:user_id] = user.id
+  end 
+  
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
